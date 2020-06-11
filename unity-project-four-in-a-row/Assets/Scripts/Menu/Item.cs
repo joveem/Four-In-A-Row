77ada51fa68168c6729d2a2083397827b07ed842
Joveem <umjoveem@gmail.com>
2020-06-10 22:41:46 -0300

    Organization

---
+++ unity-project-four-in-a-row/Assets/Scripts/Menu/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "item_00", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class Item : ScriptableObject
+{
+    public int id = 0;
+    public string item_name = "Item Name";
+    public ItemType type = ItemType.cloth;
+    public Sprite sprite;
+    public GameManager prefab;
+}
+
+public enum ItemType{
+
+    cloth,
+    skin
+
+}
