96e5683fe87e38277efb0c7c687080ab19006435
Joveem <umjoveem@gmail.com>
2020-06-12 23:25:38 -0300

    Appearance Update

--- unity-project-four-in-a-row/Assets/Scripts/Game/Appearance.cs
+++ unity-project-four-in-a-row/Assets/Scripts/Game/Appearance.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class Appearance : MonoBehaviour
 {
-    public GameManager skin;
-    public GameManager cloth;
+    public GameObject skin;
+    public GameObject cloth;
+
+    public void mudaroupa(Item item_)
+    {
+
+        CopyComponent(item_.prefab.GetComponent<SkinnedMeshRenderer>(), (int)item_.type == 0 ? skin : cloth);
+
+    }
+
+    T CopyComponent<T>(T original, GameObject destination) where T : Component
+    {
+        System.Type type = original.GetType();
+        var dst = destination.GetComponent(type) as T;
+        if (!dst) dst = destination.AddComponent(type) as T;
+        var fields = type.GetFields();
+        foreach (var field in fields)
+        {
+            if (field.IsStatic) continue;
+            field.SetValue(dst, field.GetValue(original));
+        }
+        var props = type.GetProperties();
+        foreach (var prop in props)
+        {
+            if (!prop.CanWrite || !prop.CanWrite || prop.Name == "name") continue;
+            prop.SetValue(dst, prop.GetValue(original, null), null);
+        }
+        return dst as T;
+    }
 }
